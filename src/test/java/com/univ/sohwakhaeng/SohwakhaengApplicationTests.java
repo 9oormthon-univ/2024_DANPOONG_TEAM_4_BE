7b721fea7d2118915f6f9e13d071e2da86e04900
zzdh8 <ekwls20@gmail.com>
2024-11-18 19:39:08 +0900

    project setting

---
+++ src/test/java/com/univ/sohwakhaeng/SohwakhaengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univ.sohwakhaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SohwakhaengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
