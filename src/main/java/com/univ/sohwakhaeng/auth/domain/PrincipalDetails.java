87e27d869dcfdbd868ff139db1fa80d2e973f9a5
zzdh8 <ekwls20@gmail.com>
2024-11-20 01:35:18 +0900

    #2 Feat : 로그인/로그아웃(사용자 정보 관리 클래스)
    - Security 과정에서 필요한 사용자 클래스를 구현하였다.
    - 소셜 로그인 과정에서 resource server의 데이터를 담아둘 클래스를 구현하였다.

---
+++ src/main/java/com/univ/sohwakhaeng/auth/domain/PrincipalDetails.java
@@ -0,0 +1,73 @@
+package com.univ.sohwakhaeng.auth.domain;
+
+import com.univ.sohwakhaeng.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+public class PrincipalDetails implements UserDetails, OAuth2User {
+
+    private final User user;
+    private OAuth2UserInfo oAuth2UserInfo;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    public PrincipalDetails(User user, OAuth2UserInfo oAuth2UserInfo) {
+        this.user = user;
+        this.oAuth2UserInfo = oAuth2UserInfo;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2UserInfo.getAttributes();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(
+                (GrantedAuthority) () -> user.getAuthority().toString());
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public String getName() {
+        return user.getUsername();
+    }
+}
