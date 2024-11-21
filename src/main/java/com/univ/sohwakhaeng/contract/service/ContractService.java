73108ed736e035aa4d1d8f4368594ab2c5b50f3c
zzdh8 <ekwls20@gmail.com>
2024-11-21 16:50:45 +0900

    #21 Feat : 내 계약 전체 조회 로직 추가
    - 사용자의 계약 정보를 반환하는 로직을 추하였다.
    - Transactional 어노테이션을 추가하였다.

--- src/main/java/com/univ/sohwakhaeng/contract/service/ContractService.java
+++ src/main/java/com/univ/sohwakhaeng/contract/service/ContractService.java
@@ -1,19 +1,24 @@
 package com.univ.sohwakhaeng.contract.service;
 
 import com.univ.sohwakhaeng.auth.domain.PrincipalDetails;
+import com.univ.sohwakhaeng.contract.api.dto.ContractReqDto;
+import com.univ.sohwakhaeng.contract.api.dto.ContractsInfoDto;
 import com.univ.sohwakhaeng.contract.domain.Contract;
 import com.univ.sohwakhaeng.contract.domain.ContractProducts;
 import com.univ.sohwakhaeng.contract.domain.MethodToTake;
 import com.univ.sohwakhaeng.contract.domain.repository.ContractProductsRepository;
 import com.univ.sohwakhaeng.contract.domain.repository.ContractRepository;
-import com.univ.sohwakhaeng.contract.dto.ContractReqDto;
 import com.univ.sohwakhaeng.enterprise.Enterprise;
 import com.univ.sohwakhaeng.enterprise.repository.EnterpriseRepository;
+import com.univ.sohwakhaeng.global.common.dto.PagedResponseDto;
 import com.univ.sohwakhaeng.product.Product;
 import com.univ.sohwakhaeng.product.repository.ProductRepository;
 import com.univ.sohwakhaeng.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +29,7 @@ public class ContractService {
     private final EnterpriseRepository enterpriseRepository;
     private final ProductRepository productRepository;
 
+    @Transactional
     public void acceptContract(ContractReqDto contractReqDto, PrincipalDetails principal) {
         Contract contract = extractContractEntity(contractReqDto, principal);
         contractReqDto.products().forEach(product -> {
@@ -39,7 +45,8 @@ public class ContractService {
 
     }
 
-    private Contract extractContractEntity(ContractReqDto contractReqDto, PrincipalDetails principal) {
+    @Transactional
+    protected Contract extractContractEntity(ContractReqDto contractReqDto, PrincipalDetails principal) {
         User user = principal.getUser();
         Enterprise enterprise = enterpriseRepository.findById(contractReqDto.enterpriseId())
                 .orElseThrow(() -> new IllegalArgumentException("해당 업체가 존재하지 않습니다."));
@@ -57,5 +64,25 @@ public class ContractService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 계약이 존재하지 않습니다."));
     }
 
+    @Transactional(readOnly = true)
+    public PagedResponseDto<ContractsInfoDto> getMyContracts(PrincipalDetails principal, Pageable pageable) {
+        User user = principal.getUser();
+        Page<Contract> contracts = getPagedContractsByUser(pageable, user);
+        Page<ContractsInfoDto> contractsInfoDtos = contracts.map(this::convertToDtoFromEntity);
+        return new PagedResponseDto<>(contractsInfoDtos);
+    }
 
+    private Page<Contract> getPagedContractsByUser(Pageable pageable, User user) {
+        return contractRepository.getPagedContractsByUser(user, pageable);
+    }
+
+    private ContractsInfoDto convertToDtoFromEntity(Contract contract) {
+        return ContractsInfoDto.builder()
+                .contractId(contract.getId())
+                .enterpriseId(contract.getEnterprise().getId())
+                .enterpriseName(contract.getEnterprise().getName())
+                .category(contract.getEnterprise().getCategory().toString())
+                .profileImage(contract.getEnterprise().getImageUrl())
+                .build();
+    }
 }
