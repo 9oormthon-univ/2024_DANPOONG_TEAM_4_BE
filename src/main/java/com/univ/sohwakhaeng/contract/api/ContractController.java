7c6b70ea4b0b6857279a9c0796b82a05e9b4ea04
zzdh8 <ekwls20@gmail.com>
2024-11-21 16:53:47 +0900

    #21 Feat : 계약 전체 조회 API 구현
    - 계약 전체 조회 API를 구현하였다.
    - 계약 전체 조회 성공시 응답 메세지를 추가하였다.

--- src/main/java/com/univ/sohwakhaeng/contract/api/ContractController.java
+++ src/main/java/com/univ/sohwakhaeng/contract/api/ContractController.java
@@ -1,16 +1,16 @@
 package com.univ.sohwakhaeng.contract.api;
 
 import com.univ.sohwakhaeng.auth.domain.PrincipalDetails;
-import com.univ.sohwakhaeng.contract.dto.ContractReqDto;
+import com.univ.sohwakhaeng.contract.api.dto.ContractReqDto;
+import com.univ.sohwakhaeng.contract.api.dto.ContractsInfoDto;
 import com.univ.sohwakhaeng.contract.service.ContractService;
 import com.univ.sohwakhaeng.global.common.dto.BaseResponse;
+import com.univ.sohwakhaeng.global.common.dto.PagedResponseDto;
 import com.univ.sohwakhaeng.global.common.exception.SuccessCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -23,5 +23,13 @@ public class ContractController {
     public BaseResponse<Void> requestContract(@RequestBody ContractReqDto contractReqDto, @AuthenticationPrincipal PrincipalDetails principal) {
         contractService.acceptContract(contractReqDto, principal);
         return BaseResponse.success(SuccessCode.POST_CONTRACT);
+    }
+
+    @GetMapping("/contracts")
+    public BaseResponse<PagedResponseDto<ContractsInfoDto>> getContracts(@AuthenticationPrincipal PrincipalDetails principal,
+                                                                         @RequestParam int page,
+                                                                         @RequestParam int limit
+    ) {
+        return BaseResponse.success(SuccessCode.GET_CONTRACTS, contractService.getMyContracts(principal, PageRequest.of(page, limit)));
     }
 }
