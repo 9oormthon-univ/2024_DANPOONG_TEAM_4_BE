c0c2a5b8f89a43f5ff3bd19476ac4b26d4b7f059
zzdh8 <ekwls20@gmail.com>
2024-11-23 03:22:15 +0900

    #32 Fix : 로그인 로직 변경(2)
    Redirect 코드를 추가하였다.

--- src/main/java/com/univ/sohwakhaeng/auth/api/AuthController.java
+++ src/main/java/com/univ/sohwakhaeng/auth/api/AuthController.java
@@ -3,11 +3,14 @@ package com.univ.sohwakhaeng.auth.api;
 
 import com.univ.sohwakhaeng.global.common.dto.BaseResponse;
 import com.univ.sohwakhaeng.global.common.exception.SuccessCode;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.univ.sohwakhaeng.auth.api.dto.TokenDto;
 import com.univ.sohwakhaeng.auth.service.OAuth2LoginService;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @Slf4j
@@ -17,8 +20,10 @@ public class AuthController {
     private final OAuth2LoginService oAuth2LoginService;
 
     @GetMapping("/oauth/callback/kakao")
-    public BaseResponse<TokenDto> auth(@RequestParam(value = "code") String code) {
-        return BaseResponse.success(
-                SuccessCode.USER_LOGIN_SUCCESS, oAuth2LoginService.proccessOAuth2Login(code));
+    public BaseResponse<Void> auth(@RequestParam(value = "code") String code, HttpServletResponse response) throws IOException {
+        String token = oAuth2LoginService.proccessOAuth2Login(code).accessToken();
+        String redirectUrl = "http://localhost:5137/main?token=" + token;
+        response.sendRedirect(redirectUrl);
+        return BaseResponse.success(SuccessCode.USER_LOGIN_SUCCESS);
     }
 }
