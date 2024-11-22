f1abb0809e65a6a1f4d5141f0e488ab78ac45d2e
zzdh8 <ekwls20@gmail.com>
2024-11-23 03:31:13 +0900

    #32 Fix : 포트 수정

--- src/main/java/com/univ/sohwakhaeng/auth/api/AuthController.java
+++ src/main/java/com/univ/sohwakhaeng/auth/api/AuthController.java
@@ -22,7 +22,7 @@ public class AuthController {
     @GetMapping("/oauth/callback/kakao")
     public BaseResponse<Void> auth(@RequestParam(value = "code") String code, HttpServletResponse response) throws IOException {
         String token = oAuth2LoginService.proccessOAuth2Login(code).accessToken();
-        String redirectUrl = "http://localhost:5137/main?token=" + token;
+        String redirectUrl = "http://localhost:5173/main?token=" + token;
         response.sendRedirect(redirectUrl);
         return BaseResponse.success(SuccessCode.USER_LOGIN_SUCCESS);
     }
